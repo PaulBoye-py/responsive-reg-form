d4fabb373baa90aabc03a6a698fafe43bdaf190f
unknown <paul.adeboye@yahoo.com>
2022-06-17 20:20:21 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// var input = document.getElementById('firstname');
+// input.oninvalid = function(event) {
+//     event.target.setCustomValidity('First name cannot be empty')
+// }
+
+// var input = document.getElementById('lastname');
+// input.oninvalid = function(event) {
+//     event.target.setCustomValidity('Last name cannot be empty')
+// }
